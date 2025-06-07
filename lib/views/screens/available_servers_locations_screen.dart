897ee01207a443af27e7cb537333a0cba63120c8
Md-LatifurRatul <latifurratulislam@gmail.com>
2025-06-07 17:30:22 +0600

    add vpn location

--- lib/views/screens/available_servers_locations_screen.dart
+++ lib/views/screens/available_servers_locations_screen.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:vibe_vpn/controllers/vpn_location_controller.dart';
+import 'package:vibe_vpn/views/widgets/vpn_location_card_widget.dart';
 
 class AvailableServersLocationsScreen extends StatelessWidget {
-  const AvailableServersLocationsScreen({super.key});
+  AvailableServersLocationsScreen({super.key});
+
+  final vpnLocationController = VpnLocationController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (vpnLocationController.vpnFreeServerAvailableList.isEmpty) {
+      vpnLocationController.retrieveVpnInformation();
+    }
+
+    return Obx(
+      () => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text(
+            "VPN Locations ${vpnLocationController.vpnFreeServerAvailableList.length} ",
+          ),
+        ),
+
+        body:
+            vpnLocationController.isLoadingNewLocations.value
+                ? loadingProgessIndicatorWidget()
+                : vpnLocationController.vpnFreeServerAvailableList.isEmpty
+                ? noVpnServerFoundWidget()
+                : vpnAvaibaleServersData(),
+      ),
+    );
+  }
+
+  Widget loadingProgessIndicatorWidget() {
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.redAccent),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            "Gathering Free VPN Locations",
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget noVpnServerFoundWidget() {
+    return Center(
+      child: Text(
+        "No VPNs Found, Try Again.",
+
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.black54,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget vpnAvaibaleServersData() {
+    return ListView.builder(
+      itemCount: vpnLocationController.vpnFreeServerAvailableList.length,
+      physics: BouncingScrollPhysics(),
+      padding: const EdgeInsets.all(3),
+
+      itemBuilder: (context, index) {
+        return VpnLocationCardWidget(
+          vpnInfo: vpnLocationController.vpnFreeServerAvailableList[index],
+        );
+      },
+    );
   }
 }
---
+++ lib/views/widgets/vpn_location_card_widget.dart
@@ -0,0 +1,94 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
+import 'package:vibe_vpn/controllers/home_controller.dart';
+import 'package:vibe_vpn/core/constants/app_size.dart';
+import 'package:vibe_vpn/core/models/vpn_info.dart';
+import 'package:vibe_vpn/core/theme/app_theme.dart';
+
+class VpnLocationCardWidget extends StatelessWidget {
+  const VpnLocationCardWidget({super.key, required this.vpnInfo});
+  final VpnInfo vpnInfo;
+
+  String formatSpeedBytes(int speedBytes, int decimal) {
+    if (speedBytes <= 0) {
+      return "0 B";
+    }
+    const suffixesTitle = ["Bps", "Kbps", "Mbps", "Gbps", "Tbps"];
+
+    var speedTitleIndex = (log(speedBytes) / log(1024)).floor();
+
+    return "${(speedBytes / pow(1024, speedTitleIndex)).toStringAsFixed(decimal)} ${suffixesTitle[speedTitleIndex]}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    sizeScreen = MediaQuery.of(context).size;
+
+    final homeController = Get.find<HomeController>();
+
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.symmetric(vertical: sizeScreen.height * .01),
+
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(16),
+        child: ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          leading: Container(
+            padding: const EdgeInsets.all(0.5),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black12),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Image.asset(
+              "countryFlags/${vpnInfo.countryShortName.toLowerCase()}.png",
+              height: 46,
+              width: sizeScreen.width * 0.15,
+              fit: BoxFit.cover,
+            ),
+          ),
+          //Country name
+          title: Text(vpnInfo.countryLongName),
+
+          //vpn speed
+          subtitle: Row(
+            children: [
+              Icon(Icons.shutter_speed, color: Colors.redAccent, size: 20),
+              const SizedBox(width: 4),
+              Text(
+                formatSpeedBytes(vpnInfo.speed, 2),
+
+                style: TextStyle(fontSize: 13),
+              ),
+            ],
+          ),
+
+          //number of sessions
+          trailing: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                vpnInfo.vpnSessionNum.toString(),
+
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).lightTextColor,
+                ),
+              ),
+              const SizedBox(width: 4),
+              Icon(CupertinoIcons.person_2_alt, color: Colors.redAccent),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
